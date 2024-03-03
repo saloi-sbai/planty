92cd85fb27018d027b403e4e0b5af3b3acb398e6
saloi sbai <saloi.benoum@gmail.com>
2024-03-03 21:29:32 +0100

    modif footer

--- wp-content/themes/blankslate-child/footer.php
+++ wp-content/themes/blankslate-child/footer.php
@@ -1,12 +1,12 @@
 </main>
 <footer id="footer" role="contentinfo">
-    <div id="footer">
+    
         <?php
         wp_nav_menu(array(
             'footer_menu' => 'primary_menu',
         ));
         ?>
-    </div>
+    
 </footer>
 </div>
 <?php wp_footer(); ?>
